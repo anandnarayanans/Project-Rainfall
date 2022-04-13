17d65fc41143e034c30f73a56ab2fa26b4ba1564
Anandnarayanan <90901888+anandnarayanans@users.noreply.github.com>
2022-04-13 10:20:20 +0530

    Refactored the code
    Now, it prints the rainfall data for the specified range.

--- Webscrapping_rainfall.py
+++ Webscrapping_rainfall.py
@@ -1,44 +1,46 @@
-from pandas.core.indexes.datetimes import date_range
-import requests
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 
-startDate = '2021-01-01'
-currentDate = '2021-11-07'
-rainfallDate = '2021-11-07'
+SiteUrl = 'http://122.15.179.102/ARS/home/rainfall/'
+startDate = '2022-04-01'
+currentDate = '2022-04-07'
 date_range = pd.date_range(startDate, currentDate)
 
 
 def scrapRainfallData():
     for datevalue in date_range:
         formattedDate = datevalue.strftime('%Y-%m-%d')
-        Url =  f'{formattedDate}'
-        print(f"Retreiving data for {formattedDate}")
-        webScrapping(Url)
+        Url = f'{SiteUrl}{formattedDate}'
+        print(f"Retreiving Rainfalldata for {formattedDate}\n")
+        
+        webScrapping(Url, formattedDate)
 
 
-def webScrapping(url,date):
+def webScrapping(url, date):
 
-    URL =  f'http://122.15.179.102/ARS/home/rainfall/{rainfallDate}'
-    r= requests.get(URL)
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    data_iterator = iter(soup.find_all('td'))
 
+    while True:
+        try:
+            Location = next(data_iterator).text
+            ActualMM = next(data_iterator).text
+            NormalMM = next(data_iterator).text
+            next(data_iterator).text
+            next(data_iterator).text
+            next(data_iterator).text
+            next(data_iterator).text
+            print(f'{Location} : {ActualMM} : {NormalMM}')
 
-    soup = BeautifulSoup(r.content, 'html.parser') 
+        except Exception as errordaDei:
+            print(f'{errordaDei}')
+            break
 
+        
+def main():
+    scrapRainfallData()
 
-data_iterator = iter(soup.find_all('td'))
-while True:
-            try:
-                Location = next(data_iterator).text
-                ActualMM = next(data_iterator).text
-                NormalMM = next(data_iterator).text
-                next(data_iterator).text
-                next(data_iterator).text
-                next(data_iterator).text
-                next(data_iterator).text
-                print(f'{Location} : {ActualMM} : {NormalMM}')
-
-            except Exception as err:
-                print(err)          
-
-            scrapRainfallData()
+if __name__=="__main__":
+    main()
